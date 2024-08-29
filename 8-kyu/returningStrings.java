372f7f16312308092d822a5f1a15a84e3cd53e60
Carlos Castaneda <101298482+CarlosCastanedaDev@users.noreply.github.com>
2024-08-29 16:17:31 -0500

    solution 8/29/24

---
+++ 8-kyu/returningStrings.java
@@ -0,0 +1,8 @@
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+public class returningStrings {
+    public static String greet(String name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
+
